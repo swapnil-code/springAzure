824d826aef493896a60a0ed0f276825ab4725e98
swapnil03 <swapnil03@nagarro.com>
2022-11-30 16:10:14 +0530

    first

---
+++ demo/src/main/java/com/example/cloud/demo/CloudswapApplication.java
@@ -0,0 +1,13 @@
+package com.example.cloud.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudswapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudswapApplication.class, args);
+	}
+
+}
---
+++ demo/src/main/java/com/example/cloud/demo/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.example.cloud.demo;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CloudswapApplication.class);
+	}
+
+}
